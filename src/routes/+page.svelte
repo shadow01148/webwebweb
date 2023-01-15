692491c91fb751355eb6a4899254a3cf093bf19d
Robert <76457846+shadow01148@users.noreply.github.com>
2023-01-15 21:27:37 +0800

    ben didnt like that

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,62 +4,44 @@
 	import logo from '$lib/assets/img/apple-touch-icon.png';
 </script>
 
+<html data-theme="night" class="scroll-smooth" lang="en">
+
 <div class="navbar bg-base-300">
-	<div class="flex-1">
-		<a href="/" class="btn btn-ghostnormal-case text-xl"
-			><img src={navlogo} width="250" height="300" alt="bg" /></a>
+	<div class="navbar-start">
+		<div class="flex-1">
+			<a href="/" class="btn btn-ghostnormal-case text-xl"
+				><img src={navlogo} width="250" height="300" alt="bg" /></a>
+		</div>	
 	</div>
-</div>
-
-<div class="card lg:card-side bg-base-100 shadow-xl">
-	<figure class="animate-pulse"><img src={srvicon} alt="Background" /></figure>
-	<div class="card-body">
-		<h1 class="card-title font-bold text-3xl">Yeet Hosting</h1>
-		<p class="text-xl">Cheap Servers, easy configuration</p>
-		<div class="stats stats-vertical w-72 shadow">
-			<div class="stat">
-				<div class="stat-figure text-secondary">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-				</div>
-			  <div class="stat-title">Uptime</div>
-			  <div class="stat-value text-secondary">99.999*</div>
-			</div>
-			<div class="stat">
-				<div class="stat-figure text-secondary">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-				</div>
-				<div class="stat-title">Happy Clients</div>
-				<div class="stat-value text-secondary">139</div>
-			  </div>
-			  <div class="stat">
-				<div class="stat-figure text-secondary">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-				</div>
-				<div class="stat-title">Years of Experience</div>
-				<div class="stat-value text-secondary">3</div>
-			  </div>
-		</div>
-	<div class="card-actions justify-end">
-		<a href="https://panel.mindustry.me/" class="btn bt">Get started!</a>
+	<div class="navbar-center hidden lg:flex">
+	  <ul class="menu menu-horizontal px-1">
+		<li><a href="#about">About</a></li>
+		<li><a href="https://uptime.mindustry.me/">Status</a></li>
+	  </ul>
 	</div>
-</div>
-</div>
-
-<div class="card bg-base-100 shadow-xl">
-	<div class="card-body">
-	  <h2 class="card-title font-bold text-3xl">Features</h2>
-	  <div class="card-actions justify-end">
-	  </div>
+	<div class="navbar-end">
+		<label class="swap swap-rotate">
+			<input type="checkbox" />
+			<svg class="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
+			<svg class="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>	
+		  </label>
 	</div>
   </div>
 
-<div id="about" class="card lg:card-side bg-base-200 shadow-xl">
+<div class="card lg:card-side bg-base-100 h-96 shadow-xl">
+	<div class="card-body">
+		<h1 class="font-extrabold text-5xl h-16 text-center">Secure, flexible and reliable</h1><h1  class="font-extrabold text-transparent bg-clip-text text-5xl h-16 text-center bg-gradient-to-r from-green-200 via-green-300 to-blue-500">Yeet Hosting.</h1>
+		<p class="text-3xl text-center">lol explanation here</p>
+</div>
+</div>
+
+<div id="about" class="card lg:card-side h-1/4 bg-base-200 shadow-xl">
 	<div class="divider" />
 	<figure><img src={logo} alt="About" /></figure>
 	<div class="card-body">
 		<article class="prose">
 			<h1 class="card-title font-bold text-4xl text-center">About Us</h1>
-			<div class="di vider" />
+			<div class="divider" />
 			At Yeet Hosting, we are dedicated to providing the best hosting solutions for businesses and individuals
 			alike. We understand the importance of having a reliable and efficient application, and we are
 			committed to making sure our customers have the resources they need to succeed online. Our team
@@ -120,7 +102,7 @@
 				<div class="card-body">
 					<h2 class="card-title">Minecraft Hosting</h2>
 					<div class="divider" />
-					<p>Minecraft Servers using Yeet Hosting can use up to 70 mods (depends).</p>
+					<p>Minecraft Servers using Yeet Hosting can use up to 70 mods.</p>
 				</div>
 			</div>
 			<!-- Source Engine Card -->
@@ -164,3 +146,4 @@
 		<a href="#services" class="link link-hover">Our Services</a>
 	</div>
 </footer>
+</html>
