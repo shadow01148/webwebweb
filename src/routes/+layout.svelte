13b3d1d7b06e08a57ee4a1a1ead88275d48a6ac0
Robert <76457846+shadow01148@users.noreply.github.com>
2023-01-16 21:16:25 +0800

    Tablet-friendly update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,51 @@
-<script>
+<script lang='ts'>
 	/** @type {import('./$types').LayoutData} */
 	import '../app.css';
+	import navlogo from '$lib/assets/img/logo.png';
+	import logo from '$lib/assets/img/apple-touch-icon.png';
 </script>
 
+<html data-theme="night" class="scroll-smooth" lang="en">
+
+  <div class="navbar bg-base-300">
+	<div class="navbar-start">
+	  <div class="dropdown">
+		<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+		<!-- svelte-ignore a11y-label-has-associated-control -->
+		<label tabindex="0" class="btn btn-ghost lg:hidden">
+		  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+		</label>
+		<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+		<ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+		  <li><a href="/#about">About</a></li>
+		  <li><a href="/#services">Services</a></li>
+		</ul>
+	  </div>
+	  <img class="object-cover" src={navlogo} width="250" height="300" alt="bg" />	
+	</div>
+	<div class="navbar-center hidden md:visible lg:visible lg:flex">
+		<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+		<ul tabindex="0" class="menu menu-horizontal px-1">
+			<li><a href="/#about" class="btn btn-info">About</a></li>
+			<li><a href="/#services" class="btn btn-info">Services</a></li>
+		  </ul>
+	</div>
+	<div class="navbar-end">
+	  <a href="https://discord.gg/yeet" class="btn">Get started</a>
+	</div>
+  </div>
+
 <slot />
+
+<footer class="footer p-10 bg-base-200 text-base-content">
+	<div>
+		<img alt="Logo" width="100" height="100" src={logo} />
+		<p>Yeet Hosting™.<br />The best in hosting.<br />team@mindustry.me<br />+1(702)552-2237</p>
+	</div>
+	<div>
+		<span class="footer-title">Company</span>
+		<a href="#about" class="link link-hover">About Us</a>
+		<a href="#services" class="link link-hover">Our Services</a>
+	</div>
+</footer>
+</html>	
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,14 @@
 <script lang="ts">
-	import navlogo from '$lib/assets/img/logo.png';
-	import logo from '$lib/assets/img/apple-touch-icon.png';
 	import sourceengine from '$lib/assets/img/halflife.jpg';
 	import mindus from '$lib/assets/img/mindustry.webp';
 	import mc from '$lib/assets/img/minecraft.jfif';
-
-	function switchMode() {
-		const element = document.getElementById("mood") as HTMLInputElement;
-		const input = document.getElementById("fordata");
-
-		if (element.checked) {
-			input?.setAttribute('data-theme', 'cupcake');
-			window.localStorage.setItem('theme', 'cupcake');
-		} else {
-			input?.setAttribute('data-theme', 'night');		
-			window.localStorage.setItem('theme', 'night');
-		}
-	}
-	const theme = typeof window !== 'undefined' ? localStorage.theme : null;
+	import logo from '$lib/assets/img/apple-touch-icon.png';
 </script>
-
-{@html '<!-- theme variable to be used in the future -->'}
-<html id="fordata" data-theme="" class="scroll-smooth" lang="en">
-
-<div class="navbar bg-base-300">
-	<div class="navbar-start">
-		<div class="flex-1">
-			<a href="/" class="btn btn-ghostnormal-case transition-all text-xl"
-				><img class="object-cover" src={navlogo} width="250" height="300" alt="bg" /></a>
-		</div>	
-	</div>
-	<div class="navbar-center hidden lg:flex">
-	  <ul class="menu menu-horizontal px-1">
-		<li><a href="#about">About</a></li>
-		<li><a href="https://uptime.mindustry.me/">Status</a></li>
-	  </ul>
-	</div>
-	<div class="navbar-end">
-		<label class="swap swap-rotate">
-			<input id="mood" type="checkbox" on:click={() => switchMode()} />
-			<svg class="swap-off fill-current w-14 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
-			<svg class="swap-on fill-current w-14 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>	
-		  </label>
-	</div>
-  </div>
 
 <div class="card bg-base-100 h-96 shadow-xl">
 	<div class="card-body">
-		<h1 class="font-extrabold sm:text-4xl md:text-5xl sm:h-16 md:h-16 text-center">Secure, flexible and reliable</h1><h1  class="font-extrabold sm:h-18 sm:w-18 md:h-24 text-transparent bg-clip-text sm:text-4xl md:text-5xl text-center bg-gradient-to-r from-green-200 via-green-300 to-blue-500">Yeet Hosting.</h1>
+		<h1 class="font-extrabold text-5xl sm:h-16 md:h-16 text-center">Secure, flexible and reliable</h1>
+		<h1 class="font-extrabold sm:h-20 sm:w-18 md:h-26 text-transparent bg-clip-text md:text-6xl sm:text-4xl text-center bg-gradient-to-r from-green-200 via-green-300 to-blue-500">Yeet Hosting.</h1>
 		<p class="text-3xl text-center">lol explanation here</p>
 </div>
 </div>
@@ -83,7 +44,7 @@
 	<div class="card-body">
 		<h1 class="card-title font-bold text-3xl">Services</h1>
 		<div class="divider" />
-		<div class="flex flex-row flex-wrap justify-between">
+		<div class="flex flex-row flex-wrap sm:justify-evenly md:justify-between">
 			<!-- Mindustry Card -->
 			<div class="card w-72 bg-base-100 hover:bg-base-300 shadow-xl image-full">
 				<figure>
@@ -150,18 +111,3 @@
 		</div>
 	</div>
 </div>
-
-
-<footer class="footer p-10 bg-base-200 text-base-content">
-	<div>
-		<img alt="Logo" width="100" height="100" src={logo} />
-		<p>Yeet Hosting™.<br />The best in hosting.<br />team@mindustry.me<br />+1(702)552-2237</p>
-	</div>
-	<div>
-		<span class="footer-title">Company</span>
-		<a href="#about" class="link link-hover">About Us</a>
-		<a href="#services" class="link link-hover">Our Services</a>
-	</div>
-</footer>
-
-</html>
