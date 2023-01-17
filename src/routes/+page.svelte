ad65b03cb05c80010c49251321ff40f26403d617
Robert <76457846+shadow01148@users.noreply.github.com>
2023-01-17 17:36:40 +0800

    one testimony is enough

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,47 +4,103 @@
 	import mc from '$lib/assets/img/minecraft.jfif';
 	import logo from '$lib/assets/img/apple-touch-icon.png';
 	import rt from '$lib/assets/img/testimonials/testimonials-1.jpg';
-	import schutz from '$lib/assets/img/testimonials/testimonials-2.jpg';
 </script>
 
 <div class="card bg-base-100 transition-shadow shadow-xl">
-	<div class="card-body">
-		<h1 class="font-extrabold text-4xl sm:text-2xl md:text-4xl sm:h-20 text-center">Secure, flexible and reliable</h1>
-		<h1 class="font-extrabold text-4xl sm:text-2xl md:text-6xl sm:h-20 text-transparent bg-clip-text text-center bg-gradient-to-r from-yellow-500 via-purple-500 to-blue-500">Yeet Hosting.</h1>
+	<div class="card-body text-center">
+		<h1 class="font-extrabold text-4xl sm:text-2xl md:text-4xl sm:h-20 text-center">
+			Secure, flexible and reliable
+		</h1>
+		<h1
+			class="font-extrabold text-4xl sm:text-2xl md:text-6xl md:align-top sm:h-20 text-transparent bg-clip-text text-center bg-gradient-to-r from-yellow-500 via-purple-500 to-blue-500"
+		>
+			Yeet Hosting.
+		</h1>
 		<div class="justify-center flex flex-wrap sm:justify-evenly md:justify-between">
-		<div class="card bg-base-100 sm:w-48 md:w-72 shadow-xl">
-			<div class="card-body">
-			
-			  <h2 class="card-title">
-				<svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-red-500">
-					<path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-			  	</svg>
-			   	24/7 Support
-				</h2>
+			<div class="card bg-base-100 sm:w-48 md:w-72 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6 stroke-red-500"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+							/>
+						</svg>
+						24/7 Support
+					</h2>
+				</div>
 			</div>
-		  </div>
-		  <div class="card bg-base-100 sm:w-48 md:w-72 shadow-xl">
-			<div class="card-body">
-			  <h2 class="card-title">
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-blue-500">
-					<path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-			  	</svg>
-			  	Low Ping 
-				</h2>
+			<div class="card bg-base-100 sm:w-48 md:w-72 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6 stroke-blue-500"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+							/>
+						</svg>
+						Low Ping
+					</h2>
+				</div>
 			</div>
-		  </div>
-		  <div class="card bg-base-100 sm:w-48 md:w-72 shadow-xl">
-			<div class="card-body">
-			  <h2 class="card-title">
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
-			  </svg>
-			  Fast Transfer Rate
-			  </h2>
+			<div class="card bg-base-100 sm:w-48 md:w-72 shadow-xl">
+				<div class="card-body">
+					<h2 class="card-title">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+							/>
+						</svg>
+						Fast Transfer Rate
+					</h2>
+				</div>
 			</div>
-		  </div>
-		  </div>	
-	</div>	
+		</div>
+		<div id="one" class="card-body">
+			<div class="w-full flex flex-col items-center justify-center">
+				<h1 class="text-2xl leading-relaxed text-center w-4/5 transition-all">
+					"Yeet Hosting is just amazing, it has great ping and its better than what i was previously
+					using."
+				</h1>
+				<div class="flex items-center gap-5 h-20">
+					<div class="rounded-full w-12 h-12 bg-black overflow-hidden">
+						<img alt="testimonial" src={rt} />
+					</div>
+					<div class="flex flex-col tracking-wider">
+						<!-- svelte-ignore a11y-label-has-associated-control -->
+						<label class=" font-bold text-base">RTOmega</label>
+						<!-- svelte-ignore a11y-label-has-associated-control -->
+						<label class="text-gray-400 font-normal text-sm">Omega Hub Owner</label>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div id="about" class="card lg:card-side h-1/4 bg-base-200 shadow-xl">
@@ -82,12 +138,7 @@
 			<!-- Mindustry Card -->
 			<div class="card w-72 bg-base-100 hover:bg-base-300 shadow-xl image-full">
 				<figure>
-					<img
-						src={mindus}
-						width="400"
-						height="225"
-						alt="Mindustry"
-					/>
+					<img src={mindus} width="400" height="225" alt="Mindustry" />
 				</figure>
 				<div class="card-body">
 					<h2 class="card-title">Mindustry Hosting</h2>
@@ -98,14 +149,11 @@
 					</p>
 				</div>
 			</div>
-			<br>
+			<br />
 			<!-- Minecraft Card -->
 			<div class="card w-72 bg-base-100 hover:bg-base-300 shadow-xl image-full">
 				<figure>
-					<img
-						src={mc}
-						alt="Minecraft"
-					/>
+					<img src={mc} alt="Minecraft" />
 				</figure>
 				<div class="card-body">
 					<h2 class="card-title">Minecraft Hosting</h2>
@@ -113,16 +161,11 @@
 					<p>Minecraft Servers using Yeet Hosting can use up to 70 mods.</p>
 				</div>
 			</div>
-			<br>
+			<br />
 			<!-- Source Engine Card -->
 			<div class="card w-72 bg-base-100 hover:bg-base-300 shadow-xl image-full">
 				<figure>
-					<img
-						src={sourceengine}
-						width="400"
-						height="255"
-						alt="Source Engine"
-					/>
+					<img src={sourceengine} width="400" height="255" alt="Source Engine" />
 				</figure>
 				<div class="card-body">
 					<h2 class="card-title">Source Engine Hosting</h2>
@@ -130,7 +173,7 @@
 					<p>Yeah! thats right, you can create a server on your favorite steam games!</p>
 				</div>
 			</div>
-			<br>
+			<br />
 			<!-- ETC -->
 			<div class="card w-72 bg-base-100 hover:bg-base-300 shadow-xl image-full">
 				<figure><img src={logo} width="400" height="255" alt="ETC" /></figure>
@@ -141,7 +184,7 @@
 					<p>For developers, we also offer a cheap plan to get started.</p>
 				</div>
 			</div>
-			<br>
+			<br />
 		</div>
 	</div>
 </div>
