25a6aaec988ba9f8bfd2a40ef8f21df0c7d63cd3
Robert <76457846+shadow01148@users.noreply.github.com>
2023-01-17 13:34:55 +0800

    Privacy Policy

---
+++ src/routes/policy/+page.svelte
@@ -0,0 +1,31 @@
+<div class="card bg-base-100 shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title text-center text-4xl font-extrabold">Privacy Policy</h2><div class="divider"></div>
+      <article class="prose-xl">
+        <h1 class="font-extrabold text-xl">This application collects some Personal Data from its users.</h1>
+        <p class="text-xl font-bold">Cloudflare Web Analytics</p>
+        <ul class="list-disc">
+            <li>Personal Data: Device information; language; usage data</li>
+        </ul>
+        <p class="text-xl font-bold">Google Analytics</p>
+        <ul class="list-disc">
+            <li>Personal Data: Tracker; Usage Data</li>
+        </ul>
+        <p class="text-xl font-bold">Cloudflare Web Analytics</p>
+        <ul class="list-disc">
+            <li>Personal Data: IP Address; Tracker; Device information</li>
+        </ul>
+
+        <p class="text-xl font-bold">Cloudflare - Traffic optimization and distribution</p>
+        <ul class="list-disc">
+            <li>Personal Data: Tracker</li>
+        </ul>
+        <h1 class="font-extrabold text-xl">Further information:</h1>
+        <p>CCPA: Collection of personal information about consumers aged 13 to 16+</p>
+        <p>We collect personal information of consumers between the age of 13 and 16+ and won't sell their data by any means unless they press the opt-in button.</p>
+        <h1 class="font-extrabold text-xl">Owner and Data Controller:</h1>
+        <p>VolleierYouKnow#6004, in representation of Yeet Hosting - Sam P. Bloom</p>
+        <p>Contact Email: admin@mindustry.me</p>
+      </article>
+    </div>
+</div>
