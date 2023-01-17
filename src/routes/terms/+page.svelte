1ce6681353d5d1214ca55341ad4d79874c4879d8
Robert <76457846+shadow01148@users.noreply.github.com>
2023-01-17 13:25:26 +0800

    Terms and Conditions of Use

---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,26 @@
+<div class="card bg-base-100 shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title text-center text-4xl font-extrabold">Terms and Conditions of Use</h2><div class="divider"></div>
+      <article class="prose-xl">
+        <p>By accessing this Website, accessible from https://www.mindustry.me/, you are agreeing to be bound by these Website Terms and Conditions of Use and agree that you are responsible for the agreement with any applicable local laws. If you disagree with any of these terms, you are prohibited from accessing this site. The materials contained in this Website are protected by copyright and trade mark law.</p>
+        <p>Permission is granted to temporarily download one copy of the materials on Yeet Hosting's Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
+        <ul class="list-disc">
+            <li>Modify or copy the materials</li>
+            <li>Use the materials for any commercial purpose or for any public display</li>
+            <li>Attempt to reverse engineer any software contained on Yeet Hosting's Website</li>
+            <li>Remove any copyright or other proprietary notations from the materials</li>
+            <li>Transferring the materials to another person or "mirror" the materials on any other server</li>
+        </ul>
+        <p>This will let Yeet Hosting to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format.</p>
+        <p>All the materials on Yeet Hosting’s Website are provided "as is". Yeet Hosting makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, Yeet Hosting does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.</p>
+        <p>Yeet Hosting or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on Yeet Hosting’s Website, even if Yeet Hosting or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.</p>
+        <p>The materials appearing on Yeet Hosting’s Website may include technical, typographical, or photographic errors. Yeet Hosting will not promise that any of the materials in this Website are accurate, complete, or current. Yeet Hosting may change the materials contained on its Website at any time without notice. Yeet Hosting does not make any commitment to update the materials.        </p>
+        <p>A client has the right to cancel a service at any time without reason. This does not include a refund unless an Admin says otherwise or it fits within our refund policy (24 hours). Cancellation of Service shall be done through the web portal.        </p>
+        <p>You, the client, are permitted to resell paid hosting services Yeet Hosting provides if they are used within legal boundaries. Notice of reselling is appreciated. If We don't agree with how you are reselling our products and or services, we will ask you to stop the reselling of those specified products or services.</p>
+        <p>Yeet Hosting has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by Yeet Hosting of the site. The use of any linked website is at the user’s own risk.</p>
+        <p>Yeet Hosting may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.</p>
+        <p class="font-extrabold text-2xl text-left">Any claim related to Yeet Hosting's Website shall be governed by the laws of US without regards to its conflict of law provisions.</p>
+        <!-- ... -->
+      </article>
+      </div>
+    </div>
