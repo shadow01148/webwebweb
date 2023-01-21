5730a40e46318102f90dae57d01d6a389045121f
Robert <76457846+shadow01148@users.noreply.github.com>
2023-01-22 02:10:23 +0800

    still not mobile compatible

---
+++ src/routes/pricing/+page.svelte
@@ -0,0 +1,305 @@
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Find the plan most flexible to you.</h1>
+			<p class="py-6">
+				Save money on your hosting costs with Yeet Hosting and enjoy our dedicated customer support
+				team's assistance with any questions or concerns you may have.
+			</p>
+			<a href="/pricing/mindustry/#pricing" class="btn-secondary btn"
+				>Get Started<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					class="h-5 w-5"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M10 3a.75.75 0 01.75.75v10.638l3.96-4.158a.75.75 0 111.08 1.04l-5.25 5.5a.75.75 0 01-1.08 0l-5.25-5.5a.75.75 0 111.08-1.04l3.96 4.158V3.75A.75.75 0 0110 3z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</a>
+		</div>
+	</div>
+</div>
+
+
+<div class="m-2 mx-auto flex h-full p-1 text-center sm:justify-evenly md:justify-between">
+	<div id="pricing" class="card bg-base-100 shadow-xl">
+		<div class="card-body items-center text-center">
+			<h2 class="card-title mb-4 text-3xl font-bold text-gray-50">Free</h2>
+			<p class="break text-slate-400">
+				If you're looking for a plan that's suitable for personal use, Yeet Hosting has got you
+				covered with our bare minimum option.
+			</p>
+			<div class="my-8 flex items-baseline  justify-center">
+				<span class="mr-2 text-5xl text-gray-50 font-extrabold">$0</span>
+				<span class="text-gray-500 dark:text-gray-400">/month</span>
+			</div>
+			<ul class="mb-8 space-y-4 text-left">
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>1 Core</h2>
+				</li>
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>30 I/O 500MB DISK</h2>
+				</li>
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>100MB RAM</h2>
+				</li>
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2 class="line-through">Database, Swap</h2>
+				</li>
+
+				<div class="card-actions justify-center">
+					<button class="bt btn"
+						>Get Started <svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="h-5 w-5"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</button>
+				</div>
+			</ul>
+		</div>
+	</div>
+
+	<div class="card bg-base-100 shadow-xl">
+		<div class="card-body items-center text-center">
+			<h2 class="card-title mb-4 text-3xl font-bold text-gray-50">Mindustry</h2>
+			<p class="break text-slate-400">
+			By purchasing this plan, you will be provided with the necessary resources and tools to set up and host a Mindustry server for you and others to play on.
+			</p>
+			<div class="my-8 flex items-baseline  justify-center">
+				<span class="mr-2 text-5xl text-gray-50 font-extrabold">$3</span>
+				<span class="text-gray-500 dark:text-gray-400">/month</span>
+			</div>
+			<ul class="mb-8 space-y-4 text-left">
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>2 Cores</h2>
+				</li>
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>1GB DISK, 500 I/O</h2>
+				</li>
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>500MB RAM</h2>
+				</li>
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>2 Backups</h2>
+				</li>
+
+				<div class="card-actions justify-center">
+					<button class="bt btn"
+						>Get Started <svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="h-5 w-5"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</button>
+				</div>
+			</ul>
+		</div>
+	</div>
+
+	<div class="card bg-base-100 shadow-xl">
+		<div class="card-body items-center text-center">
+			<h2 class="card-title mb-4 text-3xl font-bold text-gray-50">Minecraft</h2>
+			<p class="break text-slate-400">
+				By purchasing this plan, you will be provided with the necessary resources and tools to set up and host a Minecraft server for you and others to play on.
+			</p>
+			<div class="my-8 flex items-baseline  justify-center">
+				<span class="mr-2 text-5xl text-gray-50 font-extrabold">$6</span>
+				<span class="text-gray-500 dark:text-gray-400">/month</span>
+			</div>
+			<ul class="mb-8 space-y-4 text-left align-text-bottom">
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>4 Cores</h2>
+				</li>
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>1000 I/O 2GB DISK, 64MB Swap</h2>
+				</li>
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>5GB RAM</h2>
+				</li>
+				<li class="flex items-center space-x-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+					<h2>1 Database, 1 Backup</h2>
+				</li>
+
+				<div class="card-actions justify-center">
+					<button class="bt btn"
+						>Get Started <svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							class="h-5 w-5"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</button>
+				</div>
+			</ul>
+		</div>
+	</div>
+</div>
